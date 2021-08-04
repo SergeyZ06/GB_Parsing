73166b3c466a871362e48c4c47d9cc37df8752af
SergeyZ06 <SergeyZ06@yandex.ru>
2021-08-04 10:47:28 +0300

    The way of parsing yandex.ru has been changed (increased time between attempts and new header 'user agent' any time). Recording news to a file has been implemented via dedicated function. Other insignificant fixes, improvements and comments have been provided.

--- 4th_hometask/1th task.py
+++ 4th_hometask/1th task.py
@@ -14,6 +14,7 @@ import requests
 import time
 import datetime
 import pickle
+import random
 
 
 # Функция для парсинга новостей mail.ru
@@ -40,15 +41,12 @@ def func_parsing_mail(url):
             time.sleep(2)
             root_item = html.fromstring(requests.get(url=link, headers=header).text)
             list_dates.append(root_item.xpath(r'//span[@class="note__text breadcrumbs__text js-ago"]/@datetime'))
-            # list_sources.append(root_item.xpath(r'//span[@class="note"]/a[@class="link color_gray breadcrumbs__link"]'
-            #                                     r'/span[@class="link__text"]'))
             list_sources.append(root_item.xpath(r'//span[@class="note"]/a[@class="link color_gray breadcrumbs__link"]/'
                                                 r'@href'))
 
         # Собранную информацию о каждой новости записать в список в виде словаря
         for item_list in range(len(list_links)):
             list_news.append({
-                # 'source':   list_sources[item_list][0].text.replace('\xa0', ' '),
                 'source':   list_sources[item_list][0],
                 'title':    list_titles[item_list].text.replace('\xa0', ' '),
                 'link':     list_links[item_list],
@@ -77,8 +75,18 @@ def func_parsing_lenta(url):
                                 r'time/@datetime')
 
         for item_list in range(len(list_links)):
-            lambda_source = lambda x: fr'{x.split("/")[0]}//{x.split("/")[2]}/' if x[0:4] == fr'http' else url
-            lambda_url = lambda x: x if x[0:4] == fr'http' else fr'{url[:-1]}{x}'
+            # Если ссылка на новость не начинается с символов 'http',
+            # то значит это собственная новость lenta.ru
+            # и в поле source будет записана ссылка на сам агрегатор новостей
+            def lambda_source(x): return fr'{x.split("/")[0]}//{x.split("/")[2]}/' if x[0:4] == fr'http' else url
+
+            # Если ссылка на новость не начинается с символов 'http',
+            # то значит это внутреняя ссылка агрегатора lenta.ru.
+            # Например:
+            # /news/2021/08/04/health_covid/'
+            # должно быть преобразовано в:
+            # https://lenta.ru/news/2021/08/04/health_covid/
+            def lambda_url(x): return x if x[0:4] == fr'http' else fr'{url[:-1]}{x}'
 
             list_news.append({
                 'source':   lambda_source(list_links[item_list]),
@@ -110,13 +118,18 @@ def func_parsing_yandex(url):
 
         list_sources = []
         for link in list_links:
-            time.sleep(2)
-            root_item = html.fromstring(requests.get(url=link, headers=header).text)
+            random_sleep = random.randint(0, 90)
+            time.sleep(90 + random_sleep)
+            header_item = Headers(headers=True).generate()
+            root_item = html.fromstring(requests.get(url=link, headers=header_item).text)
             list_sources.append(root_item.xpath(r'//div[@class="news-story__head"]/a[@class="news-story__subtitle"]'
                                                 r'/@href'))
+            print(list_sources[len(list_sources) - 1])
 
         for item_list in range(len(list_titles)):
-            lambda_source = lambda x: x[0] if len(x) > 0 else ''
+            # Если yandex.ru перестал выдавать response,
+            # то записать в поле source пустую строку
+            def lambda_source(x): return x[0] if len(x) > 0 else ''
 
             list_news.append({
                 'source':   lambda_source(list_sources[item_list]),
@@ -126,6 +139,18 @@ def func_parsing_yandex(url):
             })
 
         return list_news
+
+
+# Функция для записи собранных новостей в файл
+def func_to_pickle(list_news, file_name, check=False):
+    with open(file=file_name, mode='wb') as file:
+        pickle.dump(obj=list_news, file=file)
+    # проверка записанных новостей
+    if check is True:
+        with open(file=file_name, mode='rb') as file:
+            list_news = pickle.load(file=file)
+        for item in list_news:
+            print(item)
 
 
 url_mail = fr'https://news.mail.ru/'
@@ -139,30 +164,12 @@ file_yandex = fr'data/yandex.pickle'
 
 # Парсинг и запись в файл новостей mail.ru
 list_news_mail = func_parsing_mail(url=url_mail)
-with open(file=file_mail, mode='wb') as file:
-    pickle.dump(obj=list_news_mail, file=file)
-# проверка записанных новостей
-with open(file=file_mail, mode='rb') as file:
-    list_news_mail = pickle.load(file=file)
-for item in list_news_mail:
-    print(item)
+func_to_pickle(list_news=list_news_mail, file_name=file_mail, check=True)
 
 # Парсинг и запись в файл новостей lenta.ru
 list_news_lenta = func_parsing_lenta(url=url_lenta)
-with open(file=file_lenta, mode='wb') as file:
-    pickle.dump(obj=list_news_lenta, file=file)
-# проверка записанных новостей
-with open(file=file_lenta, mode='rb') as file:
-    list_news_lenta = pickle.load(file=file)
-for item in list_news_lenta:
-    print(item)
+func_to_pickle(list_news=list_news_lenta, file_name=file_lenta, check=True)
 
 # Парсинг и запись в файл новостей yandex.ru
 list_news_yandex = func_parsing_yandex(url=url_yandex)
-with open(file=file_yandex, mode='wb') as file:
-    pickle.dump(obj=list_news_yandex, file=file)
-# проверка записанных новостей
-with open(file=file_yandex, mode='rb') as file:
-    list_news_yandex = pickle.load(file=file)
-for item in list_news_yandex:
-    print(item)
+func_to_pickle(list_news=list_news_yandex, file_name=file_yandex, check=True)
--- 4th_hometask/2th task.py
+++ 4th_hometask/2th task.py
@@ -7,39 +7,41 @@ from pymongo import MongoClient
 # Функция для получения собранной информации из файлов
 def func_load_from_pickle(file_name):
     with open(file=file_name, mode='rb') as file:
-        list_news = pickle.load(file=file)
-    return list_news
+        func_list_news = pickle.load(file=file)
+    return func_list_news
 
 
 # Функция для записи информации в БД
-def func_write_to_mongo(list_news):
+def func_write_to_mongo(func_list_news):
     mongo_db_name = f'GB_parsing'
     mongo_collection_name = f'news'
 
     # Подключение к БД
     mongo_client = MongoClient(host='localhost', port=27017)
-    mongo_db = mongo_client[mongo_db_name]
 
-    # Проверка наличия БД, если БД отсутствует
-    if mongo_db_name not in mongo_client.list_database_names():
-        # то создать коллецию в указанной БД
-        mongo_db.create_collection(mongo_collection_name)
+    if mongo_client:
+        mongo_db = mongo_client[mongo_db_name]
 
-    # Переменная для хранения списка полей коллекции
-    collection_columns = list(list_news[0].keys())
+        # Проверка наличия БД, если БД отсутствует
+        if mongo_db_name not in mongo_client.list_database_names():
+            # то создать коллецию в указанной БД
+            mongo_db.create_collection(mongo_collection_name)
 
-    # Для каждой записи в списке новостей
-    for i in range(len(list_news)):
-        # вставить в коллекцию новую запись
-        mongo_db[mongo_collection_name].insert_one({
-            '_id':  i,
-            collection_columns[0]:  list_news[i][collection_columns[0]],
-            collection_columns[1]:  list_news[i][collection_columns[1]],
-            collection_columns[2]:  list_news[i][collection_columns[2]],
-            collection_columns[3]:  list_news[i][collection_columns[3]]
-        })
+        # Переменная для хранения списка полей коллекции
+        collection_columns = list(func_list_news[0].keys())
 
-    mongo_client.close()
+        # Для каждой записи в списке новостей
+        for i in range(len(func_list_news)):
+            # вставить в коллекцию новую запись
+            mongo_db[mongo_collection_name].insert_one({
+                '_id':  i,
+                collection_columns[0]:  func_list_news[i][collection_columns[0]],
+                collection_columns[1]:  func_list_news[i][collection_columns[1]],
+                collection_columns[2]:  func_list_news[i][collection_columns[2]],
+                collection_columns[3]:  func_list_news[i][collection_columns[3]]
+            })
+
+        mongo_client.close()
 
 
 file_mail = fr'data/mail.pickle'
@@ -53,4 +55,4 @@ list_news.extend(func_load_from_pickle(file_name=file_lenta))
 list_news.extend(func_load_from_pickle(file_name=file_yandex))
 
 # Запись новостей в БД
-func_write_to_mongo(list_news=list_news)
+func_write_to_mongo(func_list_news=list_news)
